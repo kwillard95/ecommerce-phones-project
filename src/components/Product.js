8a10f38314b57a1649b4a8d45541103da10db328
kwillard95 <kwillard95@gmail.com>
2020-03-26 20:51:07 -0700

    Adds card footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,17 +6,26 @@ import ProductList from './ProductList';
 
 export default class Product extends Component {
   render() {
-    const {id, title, img, price, inCart} = this.props.product;
+    const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
           <div className="img-container p-5" onClick={() => console.log('You clicked me')}>
             <Link to="/details">
-              <img src={img} alt="product" className="card-img-top"/>
+              <img src={img} alt="product" className="card-img-top" />
             </Link>
-             <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {console.log('added to cart')}}>
-             {inCart ? (<p className="text-capitalize mb-0" disabled>in cart</p>) : (<i className="fas fa-cart-plus" />)}
-             </button>
+            <button className="cart-btn" disabled={inCart ? true : false} onClick={() => { console.log('added to cart') }}>
+              {inCart ? (<p className="text-capitalize mb-0" disabled>in cart</p>) : (<i className="fas fa-cart-plus" />)}
+            </button>
+          </div>
+          {/* card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">
+              {title}
+            </p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">${price}</span>
+            </h5>
           </div>
         </div>
         <h3>hello from product</h3>
