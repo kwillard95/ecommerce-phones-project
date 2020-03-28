e1a79ba40b4ceb85138bb8444911d8bd2a02ba56
kwillard95 <kwillard95@gmail.com>
2020-03-28 13:13:46 -0700

    Initializes empty cart componenet and implemented into cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
+import { ProductConsumer } from '../../context'
 import Title from '../Title'
 import CartColumns from './CartColumns'
+import EmptyCart from './EmptyCart'
 
 
 export default class Cart extends Component {
   render() {
     return (
-      <section>
-        <Title name="your" title="cart" />
-        <CartColumns />
-      </section>
+
+      <ProductConsumer>
+        {(value) => {
+          if (!value.cart.length) {
+            return (
+              <section>
+                <EmptyCart />
+              </section>
+            )
+          } else {
+            return (
+              <section>
+                <Title name="your" title="cart" />
+                <CartColumns />
+              </section>
+            )
+          }
+        }}
+      </ProductConsumer>
+
     )
   }
 }
