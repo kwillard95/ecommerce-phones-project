4f43fdbc6e45fc092cc5a3315253b85908730ddf
kwillard95 <kwillard95@gmail.com>
2020-03-27 20:45:36 -0700

    Adds style to cart button on details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -44,6 +44,7 @@ export default class Details extends Component {
                       </ButtonContainer>
                     </Link>
                     <ButtonContainer
+                    cart
                     disabled={inCart ? true : false}
                     onClick={() => {
                       value.addToCart(id)
