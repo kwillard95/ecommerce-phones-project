4f7a447fb77483a0ffe9e9cc7897180a6b591ff9
kwillard95 <kwillard95@gmail.com>
2020-03-27 17:06:26 -0700

    Renders static info on details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { ProductConsumer } from '../context'
 import { Link } from 'react-router-dom'
-import Title from './Title'
 import { ButtonContainer } from './Button'
 
 export default class Details extends Component {
@@ -9,7 +8,40 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {(value) => {
-          console.log(value.detailProduct)
+          const { id, company, img, price, info, title, inCart } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              {/* title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* end of title */}
+              {/* product info */}
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6
+                my-3">
+                  <img src={img} alt="product" className="img-fluid"/>
+                </div>
+                {/* product text */}
+                <div className="col-10 mx-auto col-md-6
+                my-3 text-capitalize">
+                  <h2>model : {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    made by : <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>price : ${price}</strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">some info about the product:</p>
+                  <p className="text-muted lead">
+                    {info}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
         }}
       </ProductConsumer>
     )
