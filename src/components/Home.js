df306fba26c99d6051f28ae1911c885fa88a7925
kwillard95 <kwillard95@gmail.com>
2020-03-29 00:42:20 -0700

    Adds a 3-d model of VR headset to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,84 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import React, { useCallback } from 'react'
+// import { useSpring, animated } from 'react-spring'
+
+// export default function Home() {
+
+//   const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+//   const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
+
+//   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
+
+//   return (
+//     <React.Fragment>
+//     <header id="showcase">
+//       <div className="container showcase-container">
+//         <div className="mb-1" onMouseMove={({ clientX: x, clientY: y}) => set({ xy: calc(x,y) })}>
+//         <animated.img src="img/vr-head.png" alt="vr-icon" className="vr-icon" style={{ transform: props.xy.interpolate(trans1) }}/>
+//         <h1>Welcome</h1>
+//         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi eum inventore eius ratione officiis nihil quae facilis totam fuga architecto!</p>
+//         </div>
+//       </div>
+//     </header>
+//   </React.Fragment>
+//   )
+// }
+
+import React, { Component } from "react";
+import { useSpring, animated } from 'react-spring'
+import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default class Home extends Component {
+
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+
+    let scene = new THREE.Scene();
+    let container = document.querySelector('.scene');
+    let headset;
+    let aspect = container.clientWidth / container.clientHeight;
+    let camera = new THREE.PerspectiveCamera(20, aspect, 0.1, 500);
+    camera.position.set(0,0,5);
+
+    const ambient = new THREE.AmbientLight(0x404040,3);
+    scene.add(ambient);
+
+    const light = new THREE.DirectionalLight(0xffffff,3);
+    light.position.set(10,10,30);
+    scene.add(light);
+    let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.prepend(renderer.domElement);
+
+    let loader = new GLTFLoader();
+    loader.load('3d/scene.gltf', function(gltf) {
+      scene.add(gltf.scene);
+      headset = gltf.scene.children[0];
+      animate();
+    })
+
+    function animate() {
+      requestAnimationFrame(animate);
+      headset.rotation.z += 0.005;
+      renderer.render(scene, camera);
+    }
+  }
   render() {
+
     return (
       <React.Fragment>
-        {/* <div class="menu-wrap">
-          <input type="checkbox" class="toggler"></input>
-          <div class="hamburger"><div></div></div>
-          <div class="menu">
-            <div>
-              <div>
-                <ul>
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/products">Products</Link></li>
-                  <li><Link to="/cart">Cart</Link></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <header id="showcase">
           <div className="container showcase-container">
-            <h1>Welcome</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi eum inventore eius ratione officiis nihil quae facilis totam fuga architecto!</p>
+            <div className="scene mb-1">
+              <h1>Are You Ready For A Staycation?</h1>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi eum inventore eius ratione officiis nihil quae facilis totam fuga architecto!</p>         
+            </div>
           </div>
         </header>
       </React.Fragment>
     )
   }
 }
+
 
