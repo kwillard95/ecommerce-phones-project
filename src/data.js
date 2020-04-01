8a91f0ba279a38f00f32cd3006ae9cae062f2dc9
kwillard95 <kwillard95@gmail.com>
2020-04-01 15:03:55 -0700

    Reorder data

--- src/data.js
+++ src/data.js
@@ -2,6 +2,17 @@
 export const storeProducts = [
   {
     id: 1,
+    title: "Ancient Civilizations",
+    img: "img/exp-5.png",
+    price: 220,
+    info:
+      "Have you always wondered what life was like in ancient civilizations? Well, there is no need to wonder any longer! With this virtual experience, you can choose to explore the ancient Egyptian Civilization, Greek Civilization, Chinese Civilization, or the Persian Civiliation. Travel back in time and learn about all of the things you've been wondering about! ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
     title: "Trekking The Jungle",
     img: "img/exp-1.png",
     price: 150,
@@ -12,7 +23,7 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 2,
+    id: 3,
     title: "Exploring The Sea",
     img: "img/exp-2.png",
     price: 120,
@@ -23,8 +34,8 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 3,
-    title: "Visit To Fiji",
+    id: 4,
+    title: "Visit Fiji",
     img: "img/exp-3.png",
     price: 80,
     info:
@@ -34,23 +45,12 @@ export const storeProducts = [
     total: 0
   },
   {
-    id: 4,
-    title: "Fun At A Carnival",
-    img: "img/exp-4.png",
-    price: 40,
-    info:
-      "Nothing beats the thrill of being at the highest point on a rollercoaster ride, right before the big drop! Experience a fun day at the carnival where you can get virtual face paintings, feel the sensation of riding a roller coaster, and earn awesome prizes by playing fun carnival games!",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
     id: 5,
-    title: "Ancient Civilization",
-    img: "img/exp-5.png",
-    price: 220,
+    title: "The Northern Lights",
+    img: "img/exp-8.png",
+    price: 80,
     info:
-      "Have you always wondered what life was like in ancient civilizations? Well, there is no need to wonder any longer! With this virtual experience, you can choose to explore the ancient Egyptian Civilization, Greek Civilization, Chinese Civilization, or the Persian Civiliation. Travel back in time and learn about all of the things you've been wondering about! ",
+      "Have you always dreamed of traveling to Iceland and watching the Northern Lights in the sky above you? With a specific set of conditions required to see the Northern Lights, sometimes it requires a bit of luck to come across this amazing sight. There is no need to rely on luck any longer! With this immersive experience, you can enjoy this beautiful sight from the comfort of your own home, while also learning about the science behind why this natural phenomenom occurs. It is truly an indescribable experience!",
     inCart: false,
     count: 0,
     total: 0
@@ -79,22 +79,22 @@ export const storeProducts = [
   },
   {
     id: 8,
-    title: "Northern Lights in Iceland",
-    img: "img/exp-8.png",
-    price: 80,
+    title: "A Day At Carnival",
+    img: "img/exp-4.png",
+    price: 40,
     info:
-      "Have you always dreamed of traveling to Iceland and watching the Northern Lights in the sky above you? With a specific set of conditions required to see the Northern Lights, sometimes it requires a bit of luck to come across this amazing sight. There is no need to rely on luck any longer! With this immersive experience, you can enjoy this beautiful sight from the comfort of your own home, while also learning about the science behind why this natural phenomenom occurs. It is truly an indescribable experience!",
+      "Nothing beats the thrill of being at the highest point on a rollercoaster ride, right before the big drop! Experience a fun day at the carnival where you can get virtual face paintings, feel the sensation of riding a roller coaster, and earn awesome prizes by playing fun carnival games!",
     inCart: false,
     count: 0,
     total: 0
-  }
+  }  
 ];
 
 export const detailProduct = {
   id: 1,
   title: "Trekking The Jungle",
   img: "img/exp-1.png",
-  price: 1000,
+  price: 150,
   info:
     "Explore the depths of the Amazon rainforest. The Amazon rainforest, covering much of northwestern Brazil and extending into Colombia, Peru and other South American countries, is the world’s largest tropical rainforest, famed for its biodiversity. It’s crisscrossed by thousands of rivers, including the powerful Amazon. Choose the intensity of this adventure. Do you prefer a calm walk through the forest? Perhaps you want something a little more exciting like discovering new species, escaping danger, and stumbling across secret civilizations within the forest. There is something for everyone!",
   inCart: false,
